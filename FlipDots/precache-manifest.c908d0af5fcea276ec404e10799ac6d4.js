d216c7f476d5d5918363d6f59198ffff611123a5
Owen McAteer <o.mcateer@catchdigital.com>
2020-07-15 19:05:03 +0200

    FlipDots

---
+++ FlipDots/precache-manifest.c908d0af5fcea276ec404e10799ac6d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3944e0c2e1fdb4374f39d63c1244714e",
+    "url": "/Motus-Art/FlipDot/index.html"
+  },
+  {
+    "revision": "cd51b417b397de02e595",
+    "url": "/Motus-Art/FlipDot/static/css/main.8208db66.chunk.css"
+  },
+  {
+    "revision": "963ba78c4e95d9869d25",
+    "url": "/Motus-Art/FlipDot/static/js/2.48e9a997.chunk.js"
+  },
+  {
+    "revision": "fcca2cdcc5e6967026a1ba00172f2d47",
+    "url": "/Motus-Art/FlipDot/static/js/2.48e9a997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd51b417b397de02e595",
+    "url": "/Motus-Art/FlipDot/static/js/main.2ad8afeb.chunk.js"
+  },
+  {
+    "revision": "f9cc6856c2ffeac512d9",
+    "url": "/Motus-Art/FlipDot/static/js/runtime-main.c460c353.js"
+  }
+]);
