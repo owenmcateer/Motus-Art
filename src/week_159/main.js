19a65e066c4b65e8ccd4fc7932e5774c4130e92c
Owen McAteer <o.mcateer@catchdigital.com>
2022-06-06 11:19:53 +0200

    Fixed perfect loop.

--- src/week_159/main.js
+++ src/week_159/main.js
@@ -4,12 +4,17 @@
  */
 let t = 0;
 const speed = 0.05;
+const squaresCount = 110;
 
+
+// Setup
 function setup() {
   createCanvas(1080, 1080, WEBGL);
   colorMode(RGB, 255, 255, 255, 1);
 }
 
+
+// Draw tick
 function draw() {
   background(39);
   stroke(239);
@@ -20,18 +25,19 @@ function draw() {
   rotateX(HALF_PI);
   translate(0, 50 * -54, 0);
 
-  for (let i = 0; i < 110; i++) {
+  for (let i = 0; i < squaresCount; i++) {
     push();
     translate(0, i * 40 + (t * 40));
-    stroke(239 + ((16 * i) / 110 + t * 4));
-    rotateY(i / -20 + (-t * TWO_PI) / 110);
+    stroke(239 + ((16 * i) / squaresCount + t * 4));
+    strokeWeight((i / squaresCount) * 3 + 1);
+    rotateY(i / -(squaresCount / TWO_PI) + (-t * TWO_PI) / squaresCount);
     box(500, 1, 500);
     pop();
   }
 
   // Timer
   t += speed;
-  if (t >= 1 - speed) {
+  if (t >= 1) {
     t = 0;
   }
 }
